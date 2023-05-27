949eadf658b1c5fc279c9f54861e465454ac59b4
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2023-05-27 21:40:07 +0300

    Create RAM.v

---
+++ RAM.v
@@ -0,0 +1,26 @@
+module RAM(clk, addr, write_data, write_enable, read_data); //if write = 1 then write else read
+    input  clk; //Same Clock as Register File
+    input  write_enable; //1 if instruction is store else it's equal zero
+    input  [9:0] addr; //Only take the 10 least significant bits when load/store
+    input  [15:0] write_data; 
+    output [15:0] read_data;
+    
+    reg [15:0] read_data;
+    
+    reg[15:0] Memory[1023:0];
+    
+    always@(negedge clk or addr)
+    begin
+        if(write_enable == 1) begin
+        Memory[addr] = write_data; //Used in store operation where addr = Op2 and write_data = Op1
+        end
+        
+        else if (write_enable == 0) begin
+        read_data = Memory[addr];
+        end
+    end
+endmodule
+
+
+
+            
