cbd4f3f02ccdcaac391fc18f0b7cabfbaa01edc0
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2023-06-01 22:00:17 +0300

    Update RegisterFile.v
    added testbench

--- RegisterFile.v
+++ RegisterFile.v
@@ -7,7 +7,6 @@ module RegisterFile(clk, read_addr1, read_addr2, write_addr, write_enable, write
   input  [15:0] write_data; // Used in load operation, (write_data = &Op2 from RAM)
   output  [15:0] read_data1; // Op1
   output  [15:0] read_data2;// Op1
-  input init;
   
   reg [15:0] registers [0:7]; // The eight registers comprising the register file
   
@@ -29,3 +28,76 @@ module RegisterFile(clk, read_addr1, read_addr2, write_addr, write_enable, write
   end
 
 endmodule
+
+module RegisterFile_tb;
+
+  reg clk;
+  reg [2:0] read_addr1;
+  reg [2:0] read_addr2;
+  reg [2:0] write_addr;
+  reg write_enable;
+  reg [15:0] write_data;
+  wire [15:0] read_data1;
+  wire [15:0] read_data2;
+  reg [15:0] registers [0:7];
+
+  // Instantiate the RegisterFile module
+  RegisterFile dut (
+    .clk(clk),
+    .read_addr1(read_addr1),
+    .read_addr2(read_addr2),
+    .write_addr(write_addr),
+    .write_enable(write_enable),
+    .write_data(write_data),
+    .read_data1(read_data1),
+    .read_data2(read_data2)
+  );
+
+  // Clock generation
+  always begin
+    #5 clk = ~clk;
+  end
+
+  // Test inputs
+  initial begin
+    clk = 0;
+    read_addr1 = 3'b001;    // Read address 1
+    read_addr2 = 3'b010;    // Read address 2
+    write_addr = 3'b011;    // Write address
+    write_enable = 1;       // Enable write
+    write_data = 75;  // Write data
+    #10;
+    $display("register[1]: %d", read_data1);
+    $display("register[2]: %d", read_data2);
+    $display("write_data: %d, write_addr: = %d, write_enable = %d", write_data, write_addr, write_enable);
+    
+
+    #10;
+    write_enable = 1;       // Enable write
+    write_data = 175;  // New write data
+    write_addr = 3'b100;    // Write address
+    read_addr1 = 3'b100;    // Read address 1
+    read_addr2 = 3'b011;    // Read address 2
+    #10;
+    $display("register[4]: %d", read_data1);
+    $display("register[3]: %d", read_data2);
+    $display("write_data: %d, write_addr: = %d, write_enable = %d", write_data, write_addr, write_enable);
+    
+    
+    #10;
+    write_enable = 0;       // Enable write
+    write_data = 175;  // New write data
+    write_addr = 3'b100;    // Write address
+    read_addr1 = 3'b100;    // Read address 1
+    read_addr2 = 3'b011;    // Read address 2
+    #10;
+    $display("register[4]: %d", read_data1);
+    $display("register[3]: %d", read_data2);
+    $display("write_data: %d, write_addr: = %d, write_enable = %d", write_data, write_addr, write_enable);
+
+    // End simulation
+    $finish;
+  end
+
+endmodule
+
